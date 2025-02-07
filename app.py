8c2fe4070498249580cac975a2f17ba1dbe9b49a
Yuke Wang <yuke.wang@emory.edu>
2025-02-07 12:28:15 -0600

    updates on theme, callback, tables, and figures

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def list_join(x):
         return '; '.join(x)
     else:
         return x
-
+    
 ### Incorporate data
 # recursive copy all yaml files from the shedding-hub repository;
 destination = Path.cwd()/"data"
@@ -54,6 +54,10 @@ for df in range(0,len(list_yaml)):
                              list_yaml[df]['analytes'][analyte]['reference_event']
                             ])
 df_analyte = pd.DataFrame(list_analyte, columns=['ID', 'analyte', 'biomarker', 'gene_target', 'specimen', 'unit', 'LOD', 'LOQ', 'reference_event'])
+df_analyte[' index'] = range(1, len(df_analyte) + 1)
+list_biomarker = df_analyte["biomarker"].unique()
+list_specimen = df_analyte["specimen"].unique()
+#list_gene = df_analyte["gene_target"].unique()
 
 # extract the participants information from yaml list;
 list_participant = []
@@ -68,6 +72,7 @@ for df in range(0,len(list_yaml)):
                                  key_missing(key_missing(list_yaml[df]['participants'][participant_id],'attributes'),'vaccinated') # For not required variables;
                                 ])
 df_participant = pd.DataFrame(list_participant, columns=['ID', 'participant_ID', 'age', 'sex', 'race', 'ethnicity', 'vaccinated'])
+df_participant[' index'] = range(1, len(df_participant) + 1)
 
 # extract the measurements from yaml list;
 list_measurement = []
@@ -82,38 +87,306 @@ for df in range(0,len(list_yaml)):
                                      key_missing(list_yaml[df]['participants'][participant_id]['measurements'][measurement_id],'value') # For not required variables;
                                     ])
 df_measurement = pd.DataFrame(list_measurement, columns=['ID', 'participant_ID', 'measurement_ID', 'analyte', 'time', 'value'])
+df_measurement[' index'] = range(1, len(df_measurement) + 1)
 
-### Create some static graphs
-temp_analyte = df_analyte.loc[(df_analyte["biomarker"]=="SARS-CoV-2") & (df_analyte["specimen"]=="stool") & (df_analyte["reference_event"]=="symptom onset")]
-temp_participant = df_participant.loc[df_participant['ID'].isin(temp_analyte['ID'])]
-temp_measurement = df_measurement.loc[(df_measurement['ID']+df_measurement['analyte']).isin(temp_analyte['ID']+temp_analyte['analyte'])]
+#functions to generate contents;
+def description_card():
+    """
 
-fig_scatter1 = px.scatter(temp_measurement, x='time', y='value', log_y=True, color="ID", title='SARS-CoV-2 Shedding Data for Stool Samples', labels={"time": "Days after Symptom Onset", "value": "Viral Load (gc/mL or gc/dry gram)", "ID": "Study"})
+    :return: A Div containing dashboard title & descriptions.
+    """
+    return html.Div(
+        id="description-card",
+        children=[
+            html.H5("Shedding Information Analytics"),
+            html.H3("Welcome to the Shedding Hub Dashboard"),
+            html.Div(
+                id="intro",
+                children="Explore the pathogen/biomarker shedding in different human specimens.",
+            ),
+        ],
+    )
+
+def generate_control_card():
+    """
+
+    :return: A Div containing controls for graphs.
+    """
+    return html.Div(
+        id="control-card",
+        children=[
+            html.P("Select Biomarker"),
+            dcc.Dropdown(
+                id="biomarker-select",
+                options=[{"label": i, "value": i} for i in list_biomarker],
+                value="SARS-CoV-2",
+            ),
+            html.Br(),
+            html.P("Select Specimen"),
+            dcc.Dropdown(
+                id="specimen-select",
+                #options=[{"label": i, "value": i} for i in list_specimen],
+                #value="stool",
+            ),
+            html.Br(),
+            html.Br(),
+            html.P("Select Gene Targets"),
+            dcc.Dropdown(
+                id="gene-select",
+                multi=True,
+            ),
+            html.Br(),
+            html.Div(
+                id="reset-btn-outer",
+                children=html.Button(id="reset-btn", children="Reset", n_clicks=0),
+            ),
+        ],
+    )
 
 ### Initialize the app
-app = Dash(__name__)
+app = Dash(
+    __name__,
+    meta_tags=[
+        {"name": "viewport", "content": "width=device-width, initial-scale=1"}
+    ]
+)
+app.title = "Shedding Hub Dashboard"
+
 server = app.server
 
 ### App layout
-app.layout = [
-    html.Div(children='Shedding Hub - Analytes Summarization'),
-    html.Hr(),
-    dcc.RadioItems(options=['biomarker', 'specimen', 'reference_event'], value='biomarker', inline=True, id='controls-and-radio-item'),
-    dash_table.DataTable(data=df_analyte.to_dict('records'), page_size=10),
-    dcc.Graph(figure={}, id='controls-and-graph'),
-    dcc.Graph(
-        id='scatter-plot-1',
-        figure=fig_scatter1
-    )
-]
-
-### Add controls to build the interaction
-@callback(
-    Output(component_id='controls-and-graph', component_property='figure'),
-    Input(component_id='controls-and-radio-item', component_property='value')
+app.layout = html.Div(
+    id="app-container",
+    children=[
+        # Banner
+        html.Div(
+            id="banner",
+            className="banner",
+            children=[html.Img(src=app.get_asset_url("sh_logo.png"))],
+        ),
+        # Left column
+        html.Div(
+            id="left-column",
+            className="three columns",
+            children=[description_card(), generate_control_card()],
+        ),
+        # Right column
+        html.Div(
+            id="right-column",
+            className="nine columns",
+            children=[
+                # Patient Volume Heatmap
+                html.Div(
+                    id="tables",
+                    children=[
+                        html.B("Data Tables"),
+                        html.Hr(),
+                        dcc.Tabs([
+                            dcc.Tab(label='Participant Information', children=[
+                                dash_table.DataTable(
+                                    id='table-participant-paging-and-sorting',
+                                    columns=[
+                                        {'name': i, 'id': i, 'deletable': True} for i in sorted(df_participant.columns)
+                                    ],
+                                    page_current=0,
+                                    page_size=10,
+                                    page_action='custom',
+                                    sort_action='custom',
+                                    sort_mode='single',
+                                    sort_by=[]
+                                ),
+                            ]),
+                            dcc.Tab(label='Analyte Information', children=[
+                                dash_table.DataTable(
+                                    id='table-analyte-paging-and-sorting',
+                                    columns=[
+                                        {'name': i, 'id': i, 'deletable': True} for i in sorted(df_analyte.columns)
+                                    ],
+                                    page_current=0,
+                                    page_size=10,
+                                    page_action='custom',
+                                    sort_action='custom',
+                                    sort_mode='single',
+                                    sort_by=[]
+                                ),
+                            ]),
+                            dcc.Tab(label='Measurement', children=[
+                                dash_table.DataTable(
+                                    id='table-measurement-paging-and-sorting',
+                                    columns=[
+                                        {'name': i, 'id': i, 'deletable': True} for i in sorted(df_measurement.columns)
+                                    ],
+                                    page_current=0,
+                                    page_size=10,
+                                    page_action='custom',
+                                    sort_action='custom',
+                                    sort_mode='single',
+                                    sort_by=[]
+                                ),
+                            ]),
+                        ]),
+                    ],
+                ),
+                # Patient Wait time by Department
+                html.Div(
+                    id="plots",
+                    children=[
+                        html.B("SARS-CoV-2 Shedding Data for Stool Samples"),
+                        html.Hr(),
+                        dcc.Graph(id='scatter_plot_symptom_onset'),
+                        html.Hr(),
+                        dcc.Graph(id='scatter_plot_confirmation'),
+                        html.Hr(),
+                        dcc.Graph(id='scatter_plot_enrollment'),
+                    ],
+                ),
+            ],
+        ),
+    ],
 )
-def update_graph(col_chosen):
-    fig = px.bar(pd.DataFrame({col_chosen:df_analyte[col_chosen].value_counts().index.tolist(),'count':df_analyte[col_chosen].value_counts().tolist()}),x=col_chosen, y="count")
+
+### Calls backs
+@callback(
+    Output('table-participant-paging-and-sorting', 'data'),
+    Input('table-participant-paging-and-sorting', "page_current"),
+    Input('table-participant-paging-and-sorting', "page_size"),
+    Input('table-participant-paging-and-sorting', 'sort_by'),
+)
+def update_participant_table(page_current, page_size, sort_by):
+    if len(sort_by):
+        dff_participant = df_participant.sort_values(
+            sort_by[0]['column_id'],
+            ascending=sort_by[0]['direction'] == 'asc',
+            inplace=False
+        )
+    else:
+        # No sort is applied
+        dff_participant = df_participant
+
+    return dff_participant.iloc[
+        page_current*page_size:(page_current+ 1)*page_size
+    ].to_dict('records')
+
+@callback(
+    Output('table-analyte-paging-and-sorting', 'data'),
+    Input('table-analyte-paging-and-sorting', "page_current"),
+    Input('table-analyte-paging-and-sorting', "page_size"),
+    Input('table-analyte-paging-and-sorting', 'sort_by'),
+)
+def update_analyte_table(page_current, page_size, sort_by):
+    if len(sort_by):
+        dff_analyte = df_analyte.sort_values(
+            sort_by[0]['column_id'],
+            ascending=sort_by[0]['direction'] == 'asc',
+            inplace=False
+        )
+    else:
+        # No sort is applied
+        dff_analyte = df_analyte
+
+    return dff_analyte.iloc[
+        page_current*page_size:(page_current+ 1)*page_size
+    ].to_dict('records')
+
+@callback(
+    Output('table-measurement-paging-and-sorting', 'data'),
+    Input('table-measurement-paging-and-sorting', "page_current"),
+    Input('table-measurement-paging-and-sorting', "page_size"),
+    Input('table-measurement-paging-and-sorting', 'sort_by'),
+)
+def update_measurement_table(page_current, page_size, sort_by):
+    if len(sort_by):
+        dff_measurement = df_measurement.sort_values(
+            sort_by[0]['column_id'],
+            ascending=sort_by[0]['direction'] == 'asc',
+            inplace=False
+        )
+    else:
+        # No sort is applied
+        dff_measurement = df_measurement
+
+    return dff_measurement.iloc[
+        page_current*page_size:(page_current+ 1)*page_size
+    ].to_dict('records')
+
+@app.callback(
+    [Output('specimen-select', 'options'),
+     Output('specimen-select', 'value')],
+    Input('biomarker-select', 'value')
+)
+def update_items_dropdown(selected_biomarker):
+    if selected_biomarker:
+        items = df_analyte["specimen"][df_analyte["biomarker"]==selected_biomarker].unique()
+        return [{'label': item, 'value': item} for item in items], items[0]
+    return [], []
+
+@app.callback(
+    [Output('gene-select', 'options'),
+     Output('gene-select', 'value')],
+    Input('biomarker-select', 'value')
+)
+def update_items_dropdown(selected_biomarker):
+    if selected_biomarker:
+        items = df_analyte["gene_target"][df_analyte["biomarker"]==selected_biomarker].unique()
+        return [{'label': item, 'value': item} for item in items], items
+    return [], []
+
+### Create some interactive graphs
+@callback(
+    Output('scatter_plot_symptom_onset', 'figure'),
+    Input('biomarker-select', 'value'),
+    Input('specimen-select', 'value'),
+    Input('gene-select', 'value'),
+    )
+def update_figure(selected_biomarker,selected_specimen,selected_gene):
+    filtered_df_analyte = df_analyte.loc[(df_analyte["biomarker"]==selected_biomarker) & (df_analyte["specimen"]==selected_specimen) & (df_analyte["gene_target"].isin(selected_gene)) & (df_analyte["reference_event"]=="symptom onset")]
+    filtered_df_participant = df_participant.loc[df_participant['ID'].isin(filtered_df_analyte['ID'])]
+    filtered_df_measurement = df_measurement.loc[(df_measurement['ID']+df_measurement['analyte']).isin(filtered_df_analyte['ID']+filtered_df_analyte['analyte'])]
+
+    fig = px.scatter(filtered_df_measurement, x='time', y='value', log_y=True, 
+                    color="ID", title=selected_biomarker + " Shedding Data for " + selected_specimen.capitalize() + " Samples", 
+                    labels={"time": "Days after Symptom Onset", "value": "Viral Load (gc/mL or gc/dry gram)", "ID": "Study"})
+
+    fig.update_layout(transition_duration=500)
+
+    return fig
+
+@callback(
+    Output('scatter_plot_confirmation', 'figure'),
+    Input('biomarker-select', 'value'),
+    Input('specimen-select', 'value'),
+    Input('gene-select', 'value'),
+    )
+def update_figure(selected_biomarker,selected_specimen,selected_gene):
+    filtered_df_analyte = df_analyte.loc[(df_analyte["biomarker"]==selected_biomarker) & (df_analyte["specimen"]==selected_specimen) & (df_analyte["gene_target"].isin(selected_gene)) & (df_analyte["reference_event"]=="confirmation date")]
+    filtered_df_participant = df_participant.loc[df_participant['ID'].isin(filtered_df_analyte['ID'])]
+    filtered_df_measurement = df_measurement.loc[(df_measurement['ID']+df_measurement['analyte']).isin(filtered_df_analyte['ID']+filtered_df_analyte['analyte'])]
+
+    fig = px.scatter(filtered_df_measurement, x='time', y='value', log_y=True, 
+                    color="ID",
+                    labels={"time": "Days after Confirmation", "value": "Viral Load (gc/mL or gc/dry gram)", "ID": "Study"})
+
+    fig.update_layout(transition_duration=500)
+
+    return fig
+
+@callback(
+    Output('scatter_plot_enrollment', 'figure'),
+    Input('biomarker-select', 'value'),
+    Input('specimen-select', 'value'),
+    Input('gene-select', 'value'),
+    )
+def update_figure(selected_biomarker,selected_specimen,selected_gene):
+    filtered_df_analyte = df_analyte.loc[(df_analyte["biomarker"]==selected_biomarker) & (df_analyte["specimen"]==selected_specimen) & (df_analyte["gene_target"].isin(selected_gene)) & (df_analyte["reference_event"]=="enrollment")]
+    filtered_df_participant = df_participant.loc[df_participant['ID'].isin(filtered_df_analyte['ID'])]
+    filtered_df_measurement = df_measurement.loc[(df_measurement['ID']+df_measurement['analyte']).isin(filtered_df_analyte['ID']+filtered_df_analyte['analyte'])]
+
+    fig = px.scatter(filtered_df_measurement, x='time', y='value', log_y=True, 
+                    color="ID", 
+                    labels={"time": "Days after Enrollment", "value": "Viral Load (gc/mL or gc/dry gram or gc/swab)", "ID": "Study"})
+
+    fig.update_layout(transition_duration=500)
+
     return fig
 
 ### Run the app
